703284e979ecaf8842fcf9c46441335c8c0e37a5
vlorian <vlorian.dervishi@gmail.com>
2023-07-07 14:04:00 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+function writeCards(n, m) {
+    const array = [];
+    for (let i = 0; i < n.length; i++) {
+
+        array.push(`Thank you, ${n[i]}, for the wonderful ${m} gift!`);
+    }
+    return array;
+}
+
+function countDown(w) {
+    let countDwn = n;
+    let setCounter = n;
+    while (n >= 0) {
+        console.log(w--);
+        //     if(setCounter==n){
+        //         console.log(`Expected countDown(${setCounter}) to log ${n--} first `);
+        //     }
+        //     console.log(`Expected countDown(${setCounter}) to log ${n--} after ${countDwn--}`);
+
+        //    }
+        //return countDwn;
+    }
+
+}
+
